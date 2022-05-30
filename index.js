de179aef458fa221212b2d8401294f1a31bf0962
Fontinelle <epitaciofontinelle@live.com>
2022-05-30 09:31:37 -0300

    Prettier eslint

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const app = require("express")();
+const app = require('express')();
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log("Backend...");
+  console.log('Backend...');
 });
