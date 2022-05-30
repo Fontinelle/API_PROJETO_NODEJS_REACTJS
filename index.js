784452a759cc97d8c728fd8b3fc0ae14ec63e623
Fontinelle <epitaciofontinelle@live.com>
2022-05-30 09:45:44 -0300

    bodyParser cors consign

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const consign = require('consign');
+
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(bodyParser.json());
+app.use(cors());
+
+consign({ cwd: 'src' }).into(app);
 
 app.listen(PORT, () => {
   console.log('Backend...');
